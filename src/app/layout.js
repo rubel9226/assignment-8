92a1795a204ec10325301502212bb587b16ae0b2
rubel9226 <mdrubelhossen9226@gmail.com>
2026-05-02 09:10:32 +0600

    create all json file

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/Navbar";
+import Footer from "@/Components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const OutfitFont = Outfit({
   subsets: ["latin"],
 });
 
@@ -21,12 +16,12 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${OutfitFont.className} h-full antialiased`}
     >
       <body>
         <Navbar />
         {children}
-        
+        <Footer />
       </body>
 
     </html>
